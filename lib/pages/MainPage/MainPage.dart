7da216a4b7e78359b74a3024f8c96508392f5fac
filatov04 <filka04012004@yandex.ru>
2024-12-24 04:02:23 +0300

    added first option in mainPage

---
+++ lib/pages/MainPage/MainPage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course_2024/entities/Employee/Employee.dart';
+import 'package:flutter_course_2024/entities/Last/Last.dart';
+
+class MainPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:  Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 70,
+          flexibleSpace: Container(
+            padding: EdgeInsets.only(right: 15),
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  Color.fromRGBO(5, 65, 240, 1),
+                  Color.fromRGBO(55, 235, 255, 1)
+                ]
+              )
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("lib/shared/assets/appBar/logo.png"),
+                Image.asset('lib/shared/assets/appBar/vector.png'),
+              ],
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 30),
+          child: Center(
+            child: Column(
+              children: [
+                Employee(),
+                SizedBox(height: 20),
+                Last()
+              ],
+            )
+          ),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Badge(child: Icon(Icons.notifications_sharp)),
+              label: 'Notifications',
+            ),
+            NavigationDestination(
+              icon: Badge(
+                label: Text('2'),
+                child: Icon(Icons.messenger_sharp),
+              ),
+              label: 'Messages',
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
